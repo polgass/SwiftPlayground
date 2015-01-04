da01a41a69b7d3b6d9a9f92e1a88dfdc8316e3a9
Peter Paul Sevilla <paulsevilla@Pauls-MacBook-Pro.local>
2015-01-04 21:35:11 +0800

    Add new codes for iOS 8 By Tutorials review

---
+++ iOS8ByTutorials/AdaptiveWeather/AdaptiveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by Peter Paul Sevilla on 1/4/15.
+//  Copyright (c) 2015 Peter Paul Sevilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
