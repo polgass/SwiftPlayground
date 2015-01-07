51b9b02fdd8ef88b8a1f539e167dc8a1d27af85c
Paul Sevilla <paul@onvolo.com>
2015-01-07 17:08:50 +0800

    Add RBStoryboardLink POC

---
+++ MultipleStoryboards/Pods/Target Support Files/Pods-MultipleStoryboards-RBStoryboardLink/Pods-MultipleStoryboards-RBStoryboardLink-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MultipleStoryboards_RBStoryboardLink : NSObject
+@end
+@implementation PodsDummy_Pods_MultipleStoryboards_RBStoryboardLink
+@end
