16d212af4567b574440ae262eba6ec46e3d780da
Paul Sevilla <peter@simplyhired.com>
2016-02-25 13:42:32 +0800

    Add core graphics tutorial

--- CoreGraphics_Flo/Flo/CounterView.swift
+++ CoreGraphics_Flo/Flo/CounterView.swift
@@ -83,5 +83,51 @@ let π: CGFloat = CGFloat(M_PI)
     outlineColor.setStroke()
     outlinePath.lineWidth = 5.0
     outlinePath.stroke()
+    
+    //Counter View markers
+    
+    let context = UIGraphicsGetCurrentContext()
+    
+    //1 - save original state
+    CGContextSaveGState(context)
+    outlineColor.setFill()
+    
+    let markerWidth:CGFloat = 5.0
+    let markerSize:CGFloat = 10.0
+    
+    //2 - the marker rectangle positioned at the top left
+    var markerPath = UIBezierPath(rect:
+      CGRect(x: -markerWidth/2,
+        y: 0,
+        width: markerWidth,
+        height: markerSize))
+    
+    //3 - move top left of context to the previous center position
+    CGContextTranslateCTM(context,
+      rect.width/2,
+      rect.height/2)
+    
+    for i in 1...NoOfGlasses {
+      //4 - save the centred context
+      CGContextSaveGState(context)
+      
+      //5 - calculate the rotation angle
+      var angle = arcLengthPerGlass * CGFloat(i) + startAngle - π/2
+      
+      //rotate and translate
+      CGContextRotateCTM(context, angle)
+      CGContextTranslateCTM(context,
+        0,
+        rect.height/2 - markerSize)
+      
+      //6 - fill the marker rectangle
+      markerPath.fill()
+      
+      //7 - restore the centred context for the next rotate
+      CGContextRestoreGState(context)
+    }
+    
+    //8 - restore the original state in case of more painting
+    CGContextRestoreGState(context)
   }
 }
--- CoreGraphics_Flo/Flo/ViewController.swift
+++ CoreGraphics_Flo/Flo/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
   //Counter outlets
   @IBOutlet weak var counterView: CounterView!
   @IBOutlet weak var counterLabel: UILabel!
+  @IBOutlet weak var containerView: UIView!
+  @IBOutlet weak var graphView: GraphView!
+  //Label outlets
+  @IBOutlet weak var averageWaterDrunk: UILabel!
+  @IBOutlet weak var maxLabel: UILabel!
+  
+  var isGraphViewShowing = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -26,6 +33,53 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  func setupGraphDisplay() {
+    
+    //Use 7 days for graph - can use any number,
+    //but labels and sample data are set up for 7 days
+    let noOfDays:Int = 7
+    
+    //1 - replace last day with today's actual data
+    graphView.graphPoints[graphView.graphPoints.count-1] = counterView.counter
+    
+    //2 - indicate that the graph needs to be redrawn
+    graphView.setNeedsDisplay()
+    
+    maxLabel.text = "\(graphView.graphPoints.maxElement())"
+    
+    //3 - calculate average from graphPoints
+    let average = graphView.graphPoints.reduce(0, combine: +)
+      / graphView.graphPoints.count
+    averageWaterDrunk.text = "\(average)"
+    
+    //set up labels
+    //day of week labels are set up in storyboard with tags
+    //today is last day of the array need to go backwards
+    
+    //4 - get today's day number
+    let dateFormatter = NSDateFormatter()
+    let calendar = NSCalendar.currentCalendar()
+    let componentOptions:NSCalendarUnit = .Weekday
+    let components = calendar.components(componentOptions,
+      fromDate: NSDate())
+    var weekday = components.weekday
+    
+    let days = ["S", "S", "M", "T", "W", "T", "F"]
+    
+    //5 - set up the day name labels with correct day
+    for i in (1...days.count).reverse() {
+      if let labelView = graphView.viewWithTag(i) as? UILabel {
+        if weekday == 7 {
+          weekday = 0
+        }
+        labelView.text = days[weekday--]
+        if weekday < 0 {
+          weekday = days.count - 1
+        }
+      }
+    }
+  }
+  
   @IBAction func btnPushButton(button: PushButtonView) {
     if button.isAddButton {
       counterView.counter++
@@ -35,7 +89,33 @@ class ViewController: UIViewController {
       }
     }
     counterLabel.text = String(counterView.counter)
+    
+    if isGraphViewShowing {
+      counterViewTap(nil)
+    }
   }
-
+  
+  @IBAction func counterViewTap(gesture:UITapGestureRecognizer?) {
+    if (isGraphViewShowing) {
+      
+      //hide Graph
+      UIView.transitionFromView(graphView,
+        toView: counterView,
+        duration: 1.0,
+        options: [.TransitionFlipFromLeft, .ShowHideTransitionViews],
+        completion: nil)
+    } else {
+      
+      //show Graph
+      setupGraphDisplay()
+      UIView.transitionFromView(counterView,
+        toView: graphView,
+        duration: 1.0,
+        options: [.TransitionFlipFromRight, .ShowHideTransitionViews],
+        completion: nil)
+    }
+    
+    isGraphViewShowing = !isGraphViewShowing
+  }
 }
 
